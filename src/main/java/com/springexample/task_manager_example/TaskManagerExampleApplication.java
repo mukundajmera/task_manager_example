1c573d123a819079a9ecf73792432b030dbe6c46
mukundajmera <mukundajmera94@gmail.com>
2024-08-06 00:18:56 +0530

    Initial Commit

---
+++ src/main/java/com/springexample/task_manager_example/TaskManagerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.springexample.task_manager_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springexample/task_manager_example/TaskManagerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springexample.task_manager_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
